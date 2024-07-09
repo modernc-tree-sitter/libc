7971c77dcd2b286743746778513507a9d040b9b2
Jan Mercl <0xjnml@gmail.com>
2024-07-09 12:25:45 +0200

    WIPS

--- libc_windows.go
+++ libc_windows.go
@@ -36,6 +36,7 @@ import (
 var X__imp__environ = EnvironP()
 var X__imp__wenviron = uintptr(unsafe.Pointer(&wenviron))
 var X_imp___environ = EnvironP()
+var X_imp___wenviron = uintptr(unsafe.Pointer(&wenviron))
 var X_iob [stdio.X_IOB_ENTRIES]stdio.FILE
 var Xin6addr_any [16]byte
 
