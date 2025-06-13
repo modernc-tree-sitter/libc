bf40e84b825a49d91a7f14084b61cad58c990682
Jan Mercl <0xjnml@gmail.com>
2025-06-13 14:02:31 +0200

    WIPS

--- genasm.go
+++ genasm.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 //go:build ignore
-// +build ignore
 
 // Tool for generating assembler libc wrappers.
 package main
@@ -13,6 +12,7 @@ import (
 	"fmt"
 	"go/ast"
 	"go/printer"
+	"go/types"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -33,13 +33,14 @@ const (
 
 var (
 	_ goabi0.Param = (*param)(nil)
+	_ goabi0.Slot  = (*slot)(nil)
 	_ goabi0.Type  = (*typ)(nil)
 
 	goarch = runtime.GOARCH
 	goos   = runtime.GOOS
 
-	pkg  *packages.Package // modernc.org/libc
-	word = int64(8)
+	pkg      *packages.Package // modernc.org/libc
+	wordSize = int64(8)
 )
 
 type param struct {
@@ -51,13 +52,37 @@ func (p *param) Name() string {
 	return p.name
 }
 
+type slot struct {
+	*param
+	offset int64
+}
+
+func (s *slot) Offset() int64 {
+	return s.offset
+}
+
 type typ struct {
 	alignof int64
+	elem    *typ
+	fields  []goabi0.Slot
+	len     int64
 	sizeof  int64
 }
 
 func (t *typ) Alignof() int64 {
 	return t.alignof
+}
+
+func (t *typ) Elem() goabi0.Type {
+	return t.elem
+}
+
+func (t *typ) Fields() []goabi0.Slot {
+	return t.fields
+}
+
+func (t *typ) Len() int64 {
+	return t.len
 }
 
 func (t *typ) Sizeof() int64 {
@@ -136,7 +161,7 @@ func (b *buf) Write(p []byte) (int, error) {
 func main() {
 	switch goarch {
 	case "386", "arm":
-		word = 4
+		wordSize = 4
 	}
 	cfg := &packages.Config{
 		Mode: packages.NeedName | // Package name
@@ -199,7 +224,6 @@ func main() {
 		}
 	}
 	sort.Strings(names)
-	trc("", names)
 	y := &buf{} // asm_os_arch.go
 	a := &buf{} // asm_os_arch.s
 	args := strings.Join(os.Args[1:], " ")
@@ -209,64 +233,129 @@ func main() {
 	header := fmt.Sprintf("// %s for %s/%s by '%s%v'%s\n",
 		generatedFilePrefix, goos, goarch, filepath.Base(os.Args[0]), args, generatedFileSuffix)
 	y.w("%s\npackage libc\n\n", header)
+	a.w("%s\n", header)
+	a.w("#include \"textflag.h\"\n")
 	for _, nm := range names {
 		fdn := nodes[nm]
 		y.w("func Y%s", nm[1:])
 		signature(y, fdn.Type)
 		y.w("\n")
+		in, out := inout(fdn.Type)
+		frame, args, stackIn, stackOut := goabi0.StackLayout(wordSize, in, out)
+		a.w("\n// func Y%s", nm[1:])
+		signature(a, fdn.Type)
+		a.w("\nTEXT ·Y%s(SB),$%v-%v\n", nm[1:], frame, args)
+		for _, v := range stackIn {
+			moves, err := goabi0.Cp(wordSize, v)
+			if err != nil {
+				fail(1, "%v", err)
+			}
+
+			for _, w := range moves {
+				a.w("\tMOV%s %s+%v(FP), AX\n", suffices[w.Size], w.Name, w.Offset)
+				a.w("\tMOV%s AX, %v(SP)\n", suffices[w.Size], w.Offset)
+			}
+		}
+		a.w("\tCALL ·%s(SB)\n", nm)
+		for _, v := range stackOut {
+			moves, err := goabi0.Cp(wordSize, v)
+			if err != nil {
+				fail(1, "%v", err)
+			}
+
+			for _, w := range moves {
+				a.w("\tMOV%s %v(SP), AX\n", suffices[w.Size], w.Offset)
+				a.w("\tMOV%s AX, %s+%v(FP)\n", suffices[w.Size], w.Name, w.Offset)
+			}
+		}
+		a.w("\tRET\n")
 	}
 	if err := os.WriteFile(fmt.Sprintf("asm_%s_%s.go", goos, goarch), (*bytes.Buffer)(y).Bytes(), 0660); err != nil {
 		fail(1, "%v", err)
 	}
 
-	a.w("%s\n", header)
-	a.w("#include \"textflag.h\"\n")
-	for _, nm := range names {
-		fdn := nodes[nm]
-		in, out := inout(fdn.Type)
-		frame, args, stackIn, stackOut := goabi0.StackLayout(word, in, out)
-		a.w("\n// func Y%s", nm[1:])
-		signature(a, fdn.Type)
-		a.w("\nTEXT ·Y%s(SB),$%v-%v\n", nm[1:], frame, args)
-		_ = stackIn
-		_ = stackOut
-	}
 	if err := os.WriteFile(fmt.Sprintf("asm_%s_%s.s", goos, goarch), (*bytes.Buffer)(a).Bytes(), 0660); err != nil {
 		fail(1, "%v", err)
 	}
+}
+
+var suffices = [...]string{
+	1: "B",
+	2: "W",
+	4: "L",
+	8: "Q",
 }
 
 func inout(n *ast.FuncType) (in, out []goabi0.Param) {
 	return params(n.Params, "arg"), params(n.Results, "ret")
 }
 
-func params(n *ast.FieldList, nm string) (r []goabi0.Param) {
+func params(n *ast.FieldList, defaultNm string) (r []goabi0.Param) {
 	if n == nil {
 		return nil
 	}
 
 	for _, v := range n.List {
 		t := typeof(v.Type)
+		switch tn := fmt.Sprint(v.Type); tn {
+		case "complex64":
+			ft := &typ{alignof: 4, sizeof: 4, len: -1}
+			t.fields = []goabi0.Slot{
+				&slot{offset: 0, param: &param{name: "real", typ: ft}},
+				&slot{offset: 4, param: &param{name: "imag", typ: ft}},
+			}
+		case "complex128":
+			ft := &typ{alignof: wordSize, sizeof: 8, len: -1}
+			t.fields = []goabi0.Slot{
+				&slot{offset: 0, param: &param{name: "real", typ: ft}},
+				&slot{offset: 8, param: &param{name: "imag", typ: ft}},
+			}
+		}
 		switch {
 		case len(v.Names) == 0:
-			r = append(r, &param{typ: t, name: nm})
+			r = append(r, &param{typ: t, name: defaultNm})
 		default:
 			for _, v := range v.Names {
-				r = append(r, &param{typ: t, name: v.Name})
+				r = append(r, &param{typ: t, name: rename(v.Name)})
 			}
 		}
 	}
 	return r
 }
 
-func typeof(t any) *typ {
-
+func typeof(t any) (r *typ) {
 	switch x := t.(type) {
 	case *ast.StarExpr:
-		return &typ{alignof: word, sizeof: word}
+		return &typ{alignof: wordSize, sizeof: wordSize, len: -1}
 	case *ast.Ident:
 		t := pkg.TypesInfo.Types[x].Type.Underlying()
-		return &typ{alignof: pkg.TypesSizes.Alignof(t), sizeof: pkg.TypesSizes.Sizeof(t)}
+		switch x := t.(type) {
+		case *types.Basic:
+			return typeof(x)
+		case *types.Struct:
+			r = &typ{alignof: pkg.TypesSizes.Alignof(t), sizeof: pkg.TypesSizes.Sizeof(t), len: -1}
+			var flds []*types.Var
+			for i := 0; i < x.NumFields(); i++ {
+				f := x.Field(i)
+				sz := pkg.TypesSizes.Sizeof(f.Type())
+				if sz != 0 {
+					flds = append(flds, x.Field(i))
+				}
+			}
+			offs := pkg.TypesSizes.Offsetsof(flds)
+			for i, f := range flds {
+				r.fields = append(r.fields, &slot{offset: offs[i], param: &param{name: f.Name(), typ: typeof(f.Type())}})
+			}
+			return r
+		default:
+			panic(todo("%T", x))
+		}
+	case *types.Basic:
+		return &typ{alignof: pkg.TypesSizes.Alignof(x), sizeof: pkg.TypesSizes.Sizeof(x), len: -1}
+	case *types.Alias:
+		return typeof(x.Underlying())
+	case *types.Array:
+		return &typ{alignof: pkg.TypesSizes.Alignof(x), sizeof: pkg.TypesSizes.Sizeof(x), len: x.Len(), elem: typeof(x.Elem())}
 	default:
 		panic(todo("%T", x))
 	}
@@ -277,6 +366,15 @@ func signature(b *buf, n *ast.FuncType) {
 	if n.Results != nil {
 		b.w(" ")
 		fieldList(b, n.Results, false)
+	}
+}
+
+func rename(nm string) string {
+	switch nm {
+	case "g":
+		return "g_"
+	default:
+		return nm
 	}
 }
 
@@ -300,7 +398,7 @@ func fieldList(b *buf, n *ast.FieldList, parens bool) {
 			if j != 0 {
 				b.w(", ")
 			}
-			b.w("%s", w)
+			b.w("%s", rename(w.Name))
 		}
 		if len(v.Names) != 0 {
 			b.w(" ")
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	modernc.org/cc/v4 v4.26.1
 	modernc.org/ccgo/v4 v4.28.0
 	modernc.org/fileutil v1.3.3
-	modernc.org/goabi0 v0.0.1
+	modernc.org/goabi0 v0.0.3
 	modernc.org/mathutil v1.7.1
 	modernc.org/memory v1.11.0
 )
