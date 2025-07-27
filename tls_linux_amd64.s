3c7368730e3e963c781f99b2d76db538dd04e751
Jan Mercl <0xjnml@gmail.com>
2025-07-27 16:36:38 +0200

    tls_linux_amd64.*: more API...

--- tls_linux_amd64.s
+++ tls_linux_amd64.s
@@ -27,6 +27,7 @@ TEXT ·TLSAllocaEntry(SB),$8-8
 	GO_ARGS
 	NO_LOCAL_POINTERS
 	MOVQ p0+0(FP), AX
+	MOVQ AX, 0(SP)
 	CALL ·tlsAllocaEntry(SB)
 	RET
 
@@ -34,5 +35,6 @@ TEXT ·TLSAllocaExit(SB),$8-8
 	GO_ARGS
 	NO_LOCAL_POINTERS
 	MOVQ p0+0(FP), AX
+	MOVQ AX, 0(SP)
 	CALL ·tlsAllocaExit(SB)
 	RET
